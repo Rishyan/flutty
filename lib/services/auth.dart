90a7266d4d3344e4bfcbb32b827e5639e38a63bc
Rishyan Navara <navara_r@epitech.eu>
2020-11-08 16:17:47 +0100

    added stream to look for auth change

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,14 +1,29 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutty/models/user.dart';
 
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  // user objet depuis FirebaseUser
+  User _userDeFirebase(FirebaseUser user) {
+    if (user != null) {
+      return User(uid: user.uid);
+    } else {
+      return null;
+    }
+  }
+
+  // auth change user stream
+  Stream<User> get user {
+    return _auth.onAuthStateChanged.map(_userDeFirebase);
+  }
 
   // connexion anonyme
   Future signInAnonyme() async {
     try {
       AuthResult result = await _auth.signInAnonymously();
       FirebaseUser user = result.user;
-      return user;
+      return _userDeFirebase(user);
     } catch (e) {
       print(e.toString());
       return null;

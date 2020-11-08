00f26c535b8a33ebcaaa4d68e31917849003a451
Rishyan Navara <navara_r@epitech.eu>
2020-11-08 15:42:47 +0100

    change settings.graddle, fixed sign in button and return user firebase uid

---
+++ lib/screens/authenticate/sign_in.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:flutty/services/auth.dart';
+
+class SignIn extends StatefulWidget {
+  @override
+  _SignInState createState() => _SignInState();
+}
+
+class _SignInState extends State<SignIn> {
+  final AuthService _auth = AuthService();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.brown[100],
+      appBar: AppBar(
+        backgroundColor: Colors.brown[400],
+        elevation: 0.0,
+        title: Text('Sign in to Flutty'),
+      ),
+      body: Container(
+        padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
+        child: RaisedButton(
+            child: Text('Sign in anon'),
+            onPressed: () async {
+              dynamic result = await _auth.signInAnonyme();
+              if (result == null) {
+                print('error signin in...');
+              } else {
+                print('signed sucessfull !!');
+                print(result);
+              }
+            }),
+      ),
+    );
+  }
+}
